cae0882f97136c6a25968f8e9d85beb477853f51
AR <Aakash.Rohilla89@gmail.com>
2026-01-28 01:30:10 -0500

    Initial commit Problem46.

---
+++ src/Problem46.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem46 {
+    public List<List<Integer>> permute(int[] nums) {
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Problem46 obj = new Problem46();
+
+        int[] nums = new int[]{1, 2, 3};
+        System.out.println(obj.permute(nums));
+    }
+}
