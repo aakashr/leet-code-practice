8cd3d4e6a39ba65d440811c7e9a5cad8478ddfe6
AR <Aakash.Rohilla89@gmail.com>
2026-01-28 21:32:27 -0500

    Solved Problem 46.

--- src/Problem46.java
+++ src/Problem46.java
@@ -1,12 +1,38 @@
 package src;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem46 {
     public List<List<Integer>> permute(int[] nums) {
-        return new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        for (int n:nums) {
+            list.add(n);
+        }
+
+        return permuteList(list);
+
     }
+
+    public List<List<Integer>> permuteList(List<Integer> nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.size() == 1) {
+            return Arrays.asList(nums);
+        }
+
+        for (int i=0; i<nums.size(); i++) {
+            List<List<Integer>> perms;
+            int pop = nums.removeFirst();
+            perms = permuteList(new ArrayList<>(nums));
+
+            for (List<Integer> perm:perms) {
+                perm.addLast(pop);
+                result.add(perm);
+            }
+            nums.add(pop);
+        }
+        return result;
+    }
+
 
     public static void main(String[] args) {
         Problem46 obj = new Problem46();
