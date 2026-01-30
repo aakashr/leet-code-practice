a77317b98e7f1f3293833d327eca5a146d3f51ab
AR <Aakash.Rohilla89@gmail.com>
2026-01-30 02:48:18 -0500

    Initial commit Problem 47.

--- src/Problem47.java
+++ src/Problem47.java
@@ -1,4 +1,16 @@
 package src;
 
+import java.util.*;
+
 public class Problem47 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Problem47 obj = new Problem47();
+        int[] nums = new int[]{2, 2, 1, 1};
+        System.out.println(obj.permuteUnique(nums));
+    }
 }
