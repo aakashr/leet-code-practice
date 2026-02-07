233291603133e71e0cc1c1cf64c229cf153512a6
AR <Aakash.Rohilla89@gmail.com>
2026-02-06 21:17:07 -0500

    Initial commit Problem 74

---
+++ src/Problem74.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Problem74 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem74 obj = new Problem74();
+        int[][] nums = new int[][]{{1, 1, 2}, {3, 3, 4}, {5, 5, 6}};
+        int target = 2;
+        System.out.println(obj.searchMatrix(nums, target));
+    }
+}
