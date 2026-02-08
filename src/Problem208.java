857ff21c44b33b6987dad49f35bc8fda45deac7e
AR <Aakash.Rohilla89@gmail.com>
2026-02-08 03:11:34 -0500

    Implement insert() method.

--- src/Problem208.java
+++ src/Problem208.java
@@ -1,14 +1,35 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem208 {
+    static class TrieNode {
+        char val;
+        TrieNode[] children = new TrieNode[26];
+        boolean endOfWord = false;
+
+        TrieNode(char val) {
+            this.val = val;
+        }
+    }
     static class Trie {
+        private TrieNode root;
 
         public Trie() {
-
+            root = new TrieNode(' ');
         }
 
         public void insert(String word) {
-
+            TrieNode cur = root;
+            for (char c:word.toCharArray()) {
+                int index = c - 'a';
+                if (cur.children[index] == null) {
+                    cur.children[index] = new TrieNode(c);
+                }
+                cur = cur.children[index];
+            }
+            cur.endOfWord = true;
         }
 
         public boolean search(String word) {
@@ -31,7 +52,9 @@ public class Problem208 {
     public static void main(String[] args) {
         Problem208 obj = new Problem208();
         Trie trie = new Trie();
-        String word = "apple";
-        System.out.println(trie.search(word));
+        String word1 = "apple";
+        trie.insert(word1);
+        String word2 = "april";
+        trie.insert(word2);
     }
 }
