eb78e1968dbff61fb07d36281cae12797a2c88c8
AR <Aakash.Rohilla89@gmail.com>
2026-02-10 03:23:57 -0500

    Add class TreeNode. Initial commit Problem 105.

---
+++ src/Problem105.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Problem105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        return null;
+    }
+
+    public static void main(String[] args){
+        Problem105 obj = new Problem105();
+        int[] preorder = {3,9,20,15,7};
+        int[] inorder = {9,3,15,20,7};
+        obj.buildTree(preorder, inorder);
+    }
+}
