0703a17cbb4a3ac0261c160ac9f9b6e6d9065e38
AR <Aakash.Rohilla89@gmail.com>
2026-02-11 03:40:59 -0500

    Solved Problem 105.

--- src/Problem105.java
+++ src/Problem105.java
@@ -1,15 +1,43 @@
 package src;
 
+import java.util.Arrays;
+
 public class Problem105 {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0 || inorder.length == 0) {
+            return null;
+        }
 
-        return null;
+        TreeNode root = new TreeNode(preorder[0]);
+
+        int rootIdx = 0;
+        while (rootIdx<preorder.length && inorder[rootIdx] != preorder[0]) {
+            rootIdx++;
+        }
+
+        root.left = buildTree(Arrays.copyOfRange(preorder, 1, rootIdx+1),
+                Arrays.copyOfRange(inorder, 0, rootIdx));
+        root.right = buildTree(Arrays.copyOfRange(preorder,rootIdx+1, preorder.length),
+                Arrays.copyOfRange(inorder, rootIdx+1, inorder.length));
+
+        return root;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        System.out.println(root.val);
+        dfs(root.right);
     }
 
     public static void main(String[] args){
         Problem105 obj = new Problem105();
-        int[] preorder = {3,9,20,15,7};
-        int[] inorder = {9,3,15,20,7};
-        obj.buildTree(preorder, inorder);
+
+        int[] preorder = {5,3,1,2,4,6,7};
+        int[] inorder = {1,2,3,4,5,6,7};
+
+        obj.dfs(obj.buildTree(preorder, inorder));
     }
 }
