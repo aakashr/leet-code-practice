90085d2e8b3f8b55cfcba11b008e95033506ee23
AR <Aakash.Rohilla89@gmail.com>
2026-02-16 17:01:47 -0500

    Initial commit Problem 271.

---
+++ src/Problem271.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem271 {
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        return null;
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+     Problem271 obj = new Problem271();
+     List<String> wordList = new ArrayList<>();
+     wordList.add("Hello");
+     wordList.add("World");
+     String encodedString = obj.encode(wordList);
+     List<String> decodedList = obj.decode(encodedString);
+
+    }
+}
