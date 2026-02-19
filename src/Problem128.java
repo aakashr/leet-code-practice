8e95114fd993bf11f8d196dae9cd90be624df146
AR <Aakash.Rohilla89@gmail.com>
2026-02-19 01:49:03 -0500

    Adding Problem 128 O(n) solution.

--- src/Problem128.java
+++ src/Problem128.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 public class Problem128 {
     public int longestConsecutive(int[] nums) {
+        int res = 0, curMax = 0;
         Set<Integer> s = new HashSet<>();
         for (int n: nums) {
             s.add(n);
@@ -12,33 +13,36 @@ public class Problem128 {
         if (nums.length < 1) {
             return 0;
         }
-
-        int max = nums[0];
-        for(int i=1;i<nums.length;i++) {
-            max = Math.max(max, nums[i]);
-        }
-
-        int min = nums[0];
-        for(int i=1;i<nums.length;i++) {
-            min = Math.min(min, nums[i]);
-        }
-
-        int res = 0;
-        int curMax = 0;
-        for (int i=min; i<=max;i++) {
-            if (s.contains(i)) {
+        Set<Integer> visited = new HashSet<>();
+        int j=0;
+        while (s.size() > visited.size()) {
+            int n = nums[j];
+            curMax++;
+            visited.add(n);
+            int next = n+1;
+            while (!visited.contains(next) && s.contains(next)) {
                 curMax++;
-            } else {
-                res = Math.max(res, curMax);
-                curMax = 0;
+                visited.add(next);
+                next = next+1;
             }
+            int before = n-1;
+            while (!visited.contains(before) && s.contains(before)) {
+                curMax++;
+                visited.add(before);
+                before = before-1;
+            }
+            res = Math.max(res, curMax);
+            curMax = 0;
+            j++;
         }
-        return Math.max(res, curMax);
+
+        return res;
     }
 
     public static void main(String[] args) {
         Problem128 obj = new Problem128();
-        int[] nums = new int[]{0,3,2,5,4,6,1,1};
+//        int[] nums = new int[]{0,3,2,5,4,6,1,1};
+        int[] nums = new int[]{2,20,4,10,3,4,5};
         System.out.println(obj.longestConsecutive(nums));
     }
 }
