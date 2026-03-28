8f21f68218629a5e3fb1e07f286748b223888dd4
AR <Aakash.Rohilla89@gmail.com>
2026-03-28 03:08:34 -0400

    Solved Problem 150.

--- src/Problem150.java
+++ src/Problem150.java
@@ -3,42 +3,55 @@ package src;
 import java.util.*;
 
 public class Problem150 {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> operands = new Stack<>();
+        Set<Character> operators = new HashSet<>();
+        operators.add('+');
+        operators.add('-');
+        operators.add('/');
+        operators.add('*');
 
-        for (int i=0; i<nums.length;i++) {
-            if (i>0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            int l=i+1, r= nums.length-1;
+        int len = tokens.length;
+        int i=0;
 
-            while (l<r) {
-                if (nums[l] + nums[r] + nums[i] == 0) {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[l]);
-                    temp.add(nums[r]);
-                    res.add(temp);
-                    l++;
-                    while (nums[l] == nums[l-1] && l<r) {
-                        l++;
-                    }
-                } else if (nums[l] + nums[r] + nums[i] > 0) {
-                    r--;
+        while(i < len) {
+            String curToken =  tokens[i];
+            if (isDigit(curToken) && !operators.contains(curToken)) {
+                operands.push(Integer.parseInt(curToken));
+            } else {
+                int result=0;
+                int num2 = operands.pop();
+                int num1 = operands.pop();
+
+                if (curToken.equals("+")) {
+                    result += (num1 + num2);
+                } else if (curToken.equals("-")) {
+                    result += (num1 - num2);
+                } else if (curToken.equals("*")) {
+                    result += (num1 * num2);
                 } else {
-                    l++;
+                    result += (num1 / num2);
                 }
+                operands.push(result);
             }
+            i++;
         }
-        return res;
+        return operands.pop();
+    }
+
+    public boolean isDigit(String str) {
+        try {
+            int num = Integer.parseInt(str);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
         Problem150 obj = new Problem150();
+        String[] tokens = new String[]{"1","2","+","3","*","4","-"};
 
-//        int[] nums = new int[]{-1,0,1,2,-1,-4};
-        int[] nums = new int[]{0, 0, 0};
-        System.out.println(obj.threeSum(nums));
+        System.out.println(obj.evalRPN(tokens));
     }
 }
