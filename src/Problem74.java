0c1900377188de21b811ac5340fe3cf9fb868b09
AR <Aakash.Rohilla89@gmail.com>
2026-04-06 02:12:25 -0400

    Add Binary Search solutions.

--- src/Problem74.java
+++ src/Problem74.java
@@ -2,41 +2,39 @@ package src;
 
 public class Problem74 {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int ROWS = matrix.length, COLS = matrix[0].length;
-        int top = 0, bottom = ROWS - 1, midROW=0;
+        int ROW = matrix.length;
+        int COL = matrix[0].length;
+        return binarySearch(matrix, target, 0, ROW*COL-1);
 
-        while (top <= bottom) {
-            midROW = (top + bottom)/2;
-            if (target > matrix[midROW][COLS - 1]) {
-                top = midROW + 1;
-            } else if (target < matrix[midROW][0]) {
-                bottom = midROW - 1;
-            } else {
-                break;
-            }
+    }
+
+    private boolean binarySearch(int[][] matrix, int target, int l, int r) {
+        int COL = matrix[0].length;
+        int mid = (l+r)/2;
+        int midRow = mid / COL;
+        int midCol = mid % COL;
+        int midValue = matrix [midRow][midCol];
+
+        if (target == midValue) {
+            return true;
         }
 
-        int l = 0, r = COLS - 1;
-
-        while (l <= r) {
-            int midCOL = (l + r) / 2;
-
-            if (target > matrix[midROW][midCOL]) {
-                l = midCOL + 1;
-            } else if (target < matrix[midROW][midCOL]) {
-                r = midCOL - 1;
-            } else {
-                return true;
-            }
+        if (l >= r) {
+            return false;
         }
 
-        return false;
+        if (target < midValue) {
+            return binarySearch(matrix, target, l, mid-1);
+        } else {
+            return binarySearch(matrix, target, mid+1, r);
+        }
+
     }
 
     public static void main(String[] args) {
         Problem74 obj = new Problem74();
         // target doesn't exist
-        // int[][] nums = new int[][]{{1, 1, 21}, {3, 3, 4}, {5, 5, 6}};
+//         int[][] nums = new int[][]{{1, 1, 21}, {3, 3, 4}, {5, 5, 6}};
 
         // target exists
         int[][] nums = new int[][]{{1, 1, 2}, {3, 3, 4}, {5, 5, 6}};
